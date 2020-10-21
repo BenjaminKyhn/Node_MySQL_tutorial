22f6232d61157f35daf6a1d283f91d62437106d2
Benjamin Kyhn <benkyhn@hotmail.com>
2020-10-21 13:01:22 +0200

    Intert into result object demo file

---
+++ demo_db_insert_id.js
@@ -0,0 +1,20 @@
+// get the client
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    port: 8001,
+    database: 'db'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    let sql = "INSERT INTO customers (name, address) VALUES ('Michelle', 'Blue Village 1')";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted, ID: " + result.insertId);
+    });
+});
