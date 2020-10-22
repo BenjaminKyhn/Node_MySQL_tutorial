dcb93d7d8ad229c020d6bf0f34342854c40d5d99
Benjamin Kyhn <benkyhn@hotmail.com>
2020-10-22 11:16:40 +0200

    Delete demo file

---
+++ demo_db_delete.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    port: 8001,
+    database: 'db'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
+    connection.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Number of records deleted: " + result.affectedRows);
+    });
+  });
